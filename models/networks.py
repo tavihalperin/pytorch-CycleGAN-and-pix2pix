fb45c5032b017a5d884c92890e3fe3c6e9a62caf
Tavi Halperin <tavi@lightricks.com>
2019-08-25 17:04:13 +0300

    d

--- models/networks.py
+++ models/networks.py
@@ -548,16 +548,17 @@ class NLayerDiscriminator(nn.Module):
             norm_layer      -- normalization layer
         """
         super(NLayerDiscriminator, self).__init__()
-        if type(norm_layer) == functools.partial:  # no need to use bias as BatchNorm2d has affine parameters
-            use_bias = norm_layer.func == nn.InstanceNorm2d
-        else:
-            use_bias = norm_layer == nn.InstanceNorm2d
+        # if type(norm_layer) == functools.partial:  # no need to use bias as BatchNorm2d has affine parameters
+        #     use_bias = norm_layer.func == nn.InstanceNorm2d
+        # else:
+        #     use_bias = norm_layer == nn.InstanceNorm2d
 
+        use_bias = False
         kw = 4
-        padw = 1
+        padw = 0#1
         sequence = [nn.Conv2d(input_nc, ndf, kernel_size=kw, stride=2, padding=padw), nn.LeakyReLU(0.2, True)]
         nf_mult = 1
-        nf_mult_prev = 1
+        # nf_mult_prev = 1
         for n in range(1, n_layers):  # gradually increase the number of filters
             nf_mult_prev = nf_mult
             nf_mult = min(2 ** n, 8)
